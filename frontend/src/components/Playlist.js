415f0dd3592f9b3a14aeb9249ef4f951cb9d2974
Peter Siemens <peterjsiemens@gmail.com>
2016-01-12 17:24:26 -0800

    Fixing indentation

--- frontend/src/components/Playlist.js
+++ frontend/src/components/Playlist.js
@@ -11,31 +11,31 @@ var Playlist = React.createClass({
 		return StanzaStore.getState();
 	},
 	componentWillMount: function(){
-    StanzaStore.addChangeListener(this._onChange);
-    this.updatePlaylist();
-  },
-  componentWillUnmount: function() {
-    StanzaStore.removeChangeListener(this._onChange);
-  },
-  componentDidUpdate: function(prevProps, prevState){
-  	if (prevProps.params.emotion != this.props.params.emotion) {
-  		this.updatePlaylist();
-  	}
- 	},
- 	updatePlaylist: function(){
- 		StanzaActions.toggleMenu(false);
- 		StanzaActions.updateEmotion(this.props.params.emotion);
-    StanzaActions.loadPlaylist(this.props.params.emotion);
- 	},
-  renderPlaylist: function(){
-  	var poem = StanzaStore.getCurrentPoem();
+		StanzaStore.addChangeListener(this._onChange);
+		this.updatePlaylist();
+	},
+	componentWillUnmount: function() {
+		StanzaStore.removeChangeListener(this._onChange);
+	},
+	componentDidUpdate: function(prevProps, prevState){
+		if (prevProps.params.emotion != this.props.params.emotion) {
+			this.updatePlaylist();
+		}
+	},
+	updatePlaylist: function(){
+		StanzaActions.toggleMenu(false);
+		StanzaActions.updateEmotion(this.props.params.emotion);
+		StanzaActions.loadPlaylist(this.props.params.emotion);
+	},
+	renderPlaylist: function(){
+		var poem = StanzaStore.getCurrentPoem();
 		return (
 			<div className="inner">
-        <Sidebar poem={poem} emotion={this.props.params.emotion} currentIndex={this.state.currentIndex} playlist={this.state.playlist} />
-        <Body text={poem.body} />
-      </div>
+				<Sidebar poem={poem} emotion={this.props.params.emotion} currentIndex={this.state.currentIndex} playlist={this.state.playlist} />
+				<Body text={poem.body} />
+			</div>
 		);
-  },
+	},
 	render: function(){
 		if (this.state.playlist.length){
 			return this.renderPlaylist();
@@ -43,9 +43,9 @@ var Playlist = React.createClass({
 			return (<div></div>);
 		}	
 	},
-  _onChange: function() {
-    this.setState(StanzaStore.getState());
-  }
+	_onChange: function() {
+		this.setState(StanzaStore.getState());
+	}
 });
 
 module.exports = Playlist;
