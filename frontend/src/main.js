35c6487c107038ea3157dc1fe6c43cce8c1b363b
Peter Siemens <peterjsiemens@gmail.com>
2016-01-13 12:41:54 -0800

    Use browserHistory API for frontend URL routing

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,13 +4,12 @@ var ReactDOM = require('react-dom');
 var Router = require('react-router').Router;
 var Route = require('react-router').Route;
 var IndexRoute = require('react-router').IndexRoute;
-var browserHistory = require('react-router').browserHistory;
+
+var browserHistory = require('history').createHistory();
 
 var App = require('./components/App');
 var Home = require('./components/Home');
 var Playlist = require('./components/Playlist');
-
-var wrapper = document.getElementById('wrapper');
 
 ReactDOM.render((
   <Router history={browserHistory}>
@@ -19,4 +18,4 @@ ReactDOM.render((
       <Route path="playlist/:emotion" component={Playlist} />
     </Route>
   </Router>
-), wrapper);
+), document.getElementById('wrapper'));
